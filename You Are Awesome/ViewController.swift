2695c46d833d135d48f03f3a357a0df6903c396f
Anthony Beckford <abeckford532@gmail.com>
2022-09-05 16:05:19 -0400

    Learned about Auto Layout, Auto resizing, and constraints

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+       // @IBOutlet connects an object on the interface builer (IB) canvas to your code
+       // I have connect the label object from Main.storyboard to the ViewController and called
+       // the variable messageLabel
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        messageLabel.text = ""
     }
-
+    
+    // @IBActions are functions  that perfrom swift code enclosed in the function's curlies
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+          messageLabel.text = "You are Awesome"
+          imageView.image = UIImage(named: "image0")
+      }
 
 }
 
