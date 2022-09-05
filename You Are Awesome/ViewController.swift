3f2ce0b4464e122656ce8e85a74db0ddf0882ce7
Anthony Beckford <abeckford532@gmail.com>
2022-09-05 17:36:54 -0400

    Finished Chapter 2.8 Learned about conditional statements, breakpoints and scope

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-       // @IBOutlet connects an object on the interface builer (IB) canvas to your code
-       // I have connect the label object from Main.storyboard to the ViewController and called
-       // the variable messageLabel
+    // @IBOutlet connects an object on the interface builer (IB) canvas to your code
+    // I have connect the label object from Main.storyboard to the ViewController and called
+    // the variable messageLabel
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
@@ -19,14 +19,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        messageLabel.text = ""
     }
     
     // @IBActions are functions  that perfrom swift code enclosed in the function's curlies
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-          messageLabel.text = "You are Awesome"
-          imageView.image = UIImage(named: "image0")
-      }
-
+        let awesomeMessage = "You Are Awesome"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+       if messageLabel.text == awesomeMessage {
+           messageLabel.text = greatMessage
+           imageView.image = UIImage(named: "image1")
+       } else if messageLabel.text == greatMessage {
+           messageLabel.text = bombMessage
+           imageView.image = UIImage(named: "image2")
+       } else {
+           messageLabel.text = awesomeMessage
+           imageView.image = UIImage(named: "image0")
+       }
+    }
+    
 }
 
