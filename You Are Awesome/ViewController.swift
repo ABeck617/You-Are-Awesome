17f3c4934ae94439ddc84e9268094902d9c3da6e
Anthony Beckford <abeckford532@gmail.com>
2022-09-08 18:23:46 -0400

    Got an introduction to Arrays, learned about declaring, initializing, adding and removing elements

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +26,15 @@ class ViewController: UIViewController {
     
     // @IBActions are functions  that perfrom swift code enclosed in the function's curlies
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages = ["You are Awesome", "You Are Great", "You Are Fabolous", "Fabolous? That's You!"]
         
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
         
-        print(imageNumber)
+       
         let imageName = "image\(imageNumber)"
         imageView.image = UIImage(named: imageName)
         imageNumber += 1
